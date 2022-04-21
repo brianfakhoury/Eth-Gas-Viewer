c61717533e6c32c8708ca44f5ad48ebae5217f3c
Brian Fakhoury <brianfakhoury@MacBook-Seneca.local>
2022-04-21 16:56:55 -0400

    add formating

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 import Web3 from "web3";
 import chalk from "chalk";
+import boxen from "boxen";
 
 // connect to geth websocket.
 // I recommend running $ geth --syncmode "light" --ws
 const web3 = new Web3("ws://127.0.0.1:8546");
 
-let prev_block; // this is used as a sequential check
-let prev_gas_price;
 let sub = web3.eth.subscribe("newBlockHeaders");
 
 // Console output string builder
 const buildInfoString = (block, utilization, gas_price) =>
-  `${new Date().toLocaleTimeString()} | New Block (${block}), Total Gas Used ${chalk.bgWhiteBright.bold.redBright(
+  `New Block (${block})
+Total Gas Used ${chalk.bgWhiteBright.bold.redBright(
     Math.round(utilization * 100) + "%"
-  )}, Upcoming Base Fee will be ${chalk.bgWhiteBright.bold.redBright(
+  )}
+Upcoming Base Fee will be ${chalk.bgWhiteBright.bold.redBright(
     Math.round((100 * gas_price) / 1e9) / 100 + " Gwei"
   )}.
   `;
@@ -24,18 +25,22 @@ sub.on("connected", function (subscriptionId) {
 
 sub.on("data", function (blockHeader) {
   // can't do anything unless we know the previous blocks data
-  const incremental_block = prev_block === blockHeader.number - 1;
 
   const utilization = blockHeader.gasUsed / blockHeader.gasLimit;
   // calculate next block base gas using EIP1559
-  const next_gas_price = incremental_block
-    ? blockHeader.baseFeePerGas * (1 + (0.125 * (utilization - 0.5)) / 0.5)
-    : "N/A";
+  const next_gas_price =
+    blockHeader.baseFeePerGas * (1 + (0.125 * (utilization - 0.5)) / 0.5);
 
-  prev_block = blockHeader.number;
-  prev_gas_price = blockHeader.baseFeePerGas;
-
-  console.log(buildInfoString(blockHeader.number, utilization, next_gas_price));
+  console.clear();
+  console.log(
+    boxen(buildInfoString(blockHeader.number, utilization, next_gas_price), {
+      title: `Updated: ${new Date().toLocaleTimeString()}`,
+      titleAlignment: "center",
+      borderStyle: "double",
+      padding: 1,
+      float: "center",
+    })
+  );
 });
 
 sub.on("error", ({ reason }) => console.error(reason));
